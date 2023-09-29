5a71f7b8b879dcf4e6fbc4ca0f721d330f14affd
Badhon303 <badhon.alam303@gmail.com>
2023-09-29 20:39:55 +0600

    backend ip added

--- src/app/[lang]/device-management/page.tsx
+++ src/app/[lang]/device-management/page.tsx
@@ -1,10 +1,10 @@
 import { columns } from "@/app/[lang]/device-management/columns"
 import { DataTable } from "@/app/[lang]/device-management/data-table"
-// import getData from "@/actions/getDevicesData"
-import getDeviceDataLocal from "@/actions/getDevicesDataLocal"
+import getData from "@/actions/getDevicesData"
+// import getDeviceDataLocal from "@/actions/getDevicesDataLocal"
 
 export default async function DeviceManagement() {
-  // const data = await getData()
-  const data = await getDeviceDataLocal()
+  const data = await getData()
+  // const data = await getDeviceDataLocal()
   return <DataTable columns={columns} data={data} />
 }
